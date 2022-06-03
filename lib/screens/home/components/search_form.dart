4b57d4b0f38e8dd67aedb3259988e91934519f32
Muhammad Yahyo <san4o.original@gamil.com>
2022-06-03 19:12:36 +0500

    errors fixed

--- lib/screens/home/components/search_form.dart
+++ lib/screens/home/components/search_form.dart
@@ -34,7 +34,7 @@ class SearchForm extends StatelessWidget {
               height: 48,
               child: ElevatedButton(
                 style: ElevatedButton.styleFrom(
-                  primary: Color(0xFFF67952),
+                  primary: const Color(0xFFF67952),
                   shape: const RoundedRectangleBorder(
                     borderRadius: BorderRadius.all(
                       Radius.circular(12),
